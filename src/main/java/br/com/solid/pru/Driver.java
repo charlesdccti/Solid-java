c00f7219fe9bc6493df11a28a463d3f0955117ff
Charles  Ferreira <charlesdccti@gmail.com>
2021-09-08 12:22:03 -0300

    Add Open–closed principle

--- src/main/java/br/com/solid/pru/Driver.java
+++ src/main/java/br/com/solid/pru/Driver.java
@@ -5,4 +5,8 @@ public class Driver extends Employee {
 	public void drive() {
 		System.out.println(super.getName() + " is driving");
 	}
+	
+	public void drive(String vehicle) {
+		System.out.println(super.getName() + " is driving a "+ vehicle);
+	}
 }
